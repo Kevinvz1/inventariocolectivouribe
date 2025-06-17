0574edabe4028f92623698f0ab948783af9dfabc
Neosphers0863 <Estivenvalencia0863@gmail.com>
2025-06-17 15:12:07 -0500

    feat se crea proceso 1 asociado al menu

---
+++ src/main/java/org/example/juanjosegallego/Inventario.java
@@ -0,0 +1,38 @@
+package org.example.juanjosegallego;
+
+import java.util.Scanner;
+
+public class Inventario {
+    public static void main(String[] args) {
+
+        //ENTRADAS
+        Scanner keyEntry = new Scanner(System.in);
+        Integer menuOption=null;
+        String colorVerde="\u001B[32m";
+
+
+        //PROCESO 1 (Crear el menu de opciones)
+        System.out.println("\n******************");
+        System.out.println("***** Uribe APP *****");
+        System.out.println("******************");
+
+        System.out.println("\n👕 Bienvenido a gestor de prendas...\n");
+        System.out.println("¿Qué quieres realizar?");
+        System.out.println(colorVerde+"1️⃣  Guardar una prenda en BD 📝");
+        System.out.println("2️⃣  Mostrar el inventario de prendas 📦");
+        System.out.println("3️⃣  Modificar los datos de una prenda ✏️");
+        System.out.println("4️⃣  Eliminar una prenda del sistema 🗑️");
+        System.out.println("5️⃣  Presiona 5 para SALIR ❌");
+
+        while(menuOption!=5){
+            System.out.println("oe");
+        }
+
+        //PROCESO 2
+
+        //PROCESO 3
+
+        //SALIDAS
+
+    }
+}
